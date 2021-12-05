29d529d2c9ac99759688ab3bf3a8eda70440d76c
Espen <esj009@uit.no>
2021-12-05 04:34:55 +0100

    yep

--- bar.sh
+++ bar.sh
@@ -3,9 +3,7 @@ source ~/dwm_test/dwm-6.2/themes/onedark.h
 function gamer(){
     echo "^c$black^ ^b$darkblue^ 󱑆 ^c$black^^b$blue^ $(date '+%I:%M')"
 }
-echo -e "Hello,\nWorld!"
 
 while true; do
      sleep 1 && xsetroot -name "$(gamer)"
-    
 done
